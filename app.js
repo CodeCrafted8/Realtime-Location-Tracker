508db48d53f7b0360437bd291f62088368e3070f
CodeCrafted7 <141803885+CodeCrafted7@users.noreply.github.com>
2024-08-19 09:27:44 +0530

    After Updating the Dotenv

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
-const server  = http.createServer(app);
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
 const io = socketio(server);
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
+
 io.on("connection", function(socket) {
-    socket.on("send-location",function(data){
-        io.emit("recieve-location",{id:socket.id, ...data});
+    console.log('A user connected:', socket.id);
+
+    socket.on("send-location", function(data) {
+        io.emit("receive-location", { id: socket.id, ...data });
     });
-    socket.on("disconnect",function(){
-        io.emit("user-disconnected",socket.id); 
-    })
+
+    socket.on("disconnect", function() {
+        console.log('User disconnected:', socket.id);
+        io.emit("user-disconnected", socket.id);
+    });
 });
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.render("index");
 });
 
-server.listen(4669);
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
